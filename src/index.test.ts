85359517c04ef428cee77ba889126c6a9a095b18
Francois Best <francois@francoisbest.com>
2020-03-14 14:29:34 +0100

    feat: Initial commit
    Extracted code from monorepo

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,33 @@
-import hello from './index'
+import { CounterMap, BrowserEventsProcessor } from './index'
+import { generateFakeSession } from '@chiffre/analytics-faker'
 
-test('testing works', () => {
-  expect(hello('World')).toEqual('Hello, World !')
+test('CounterMap', () => {
+  const map = new CounterMap()
+  map.count('foo')
+  map.count('foo')
+  map.count('foo')
+  map.count('bar')
+  map.count('bar')
+  expect(map.leaderboard).toEqual([
+    {
+      key: 'foo',
+      score: 3,
+      percent: 60
+    },
+    {
+      key: 'bar',
+      score: 2,
+      percent: 40
+    }
+  ])
+})
+
+test('BrowserEventsProcessor', () => {
+  const bep = new BrowserEventsProcessor()
+  const events = Array(10)
+    .fill(0)
+    .flatMap(() => generateFakeSession(Date.now()))
+    .sort((a, b) => a.time - b.time)
+  events.forEach(event => bep.process(event))
+  expect(bep.sessions.size).toEqual(10)
 })
